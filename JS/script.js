dfd7cfe93410075ed00c1d80d0d4b5e0ea1c48ac
saraagomezz <gomezsara.epet20@>
2022-10-06 15:24:44 -0300

    Iniciando proyecto

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+
+var array = ["Gómez", "Sara", "Pesadilla suprema"];
+console.log (array);
+
+var [a, b, c] = [Sara, Gómez, Pesadilla]; 
+console.log ("nombre" + a);
+//#region suma
+let num1S=14; 
+let num2S=2; 
+function suma (num1S, num2S){
+    return (num1S+num2S);
+} 
+console.log(suma(num1S, num2S));
+//#endregion
+
+//#region resta
+let num1R=20;
+let num2R=10;
+function resta (num1R, num2R){
+    return (num1R-num2R);
+}
+console.log(resta(num1R, num2R));
+//#endregion
+
+//#region mult
+let num1M= 8; 
+let num2M= 5; 
+
+function multip (num1M, num2M){
+    return (num1M*num2M);
+}
+console.log(multip(num1M,num2M));
+
+//#region div
+let num1D=70; 
+let num2D=2; 
+function div(num1D, num2D) {
+    return (num1D / num2D);
+}
+console.log(div(num1D, num2D));
+
+//#endregion
